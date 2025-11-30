5eeab32992e8c52ed7fffb1402d24b19a9ec87db
Shivam Raj <shi821vam@gmail.com>
2025-11-30 03:39:24 +0000

    drag process continued

--- kanban_JS/logic.js
+++ kanban_JS/logic.js
@@ -4,11 +4,37 @@ const TodoTask = document.getElementById("todo")
 const addBtn = document.getElementById("kanban-btn");
 const taskDiv = document.querySelectorAll(".task")
 
+let draggedElement = null;
+
 
 // draging the div from one board to another
 taskDiv.forEach(task => {
     task.addEventListener("drag", function (e) {
-        console.log("drag", e)
+        // console.log("drag", e)
+        draggedElement = this;
     })
 })
+
+function addDragEventsOnColumn(column) {
+    column.addEventListener("dragenter", function (e) {
+        this.classList.add("hover-over")
+    })
+
+    column.addEventListener("dragleave", function (e) {
+        this.classList.remove("hover-over")
+    })
+
+    column.addEventListener("dragover", function (e) {
+        e.preventDefault();
+    })
+
+    column.addEventListener("drop", function (e) {
+        e.preventDefault();
+        this.classList.remove("hover-over")
+    })
+}
+
+addDragEventsOnColumn(TodoTask)
+addDragEventsOnColumn(ProgressTask)
+addDragEventsOnColumn(DoneTask)
 
